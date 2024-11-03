605d27f47ce3e81b837a5cfc446766002ac7b1cb
sifes <lonfark@gmail.com>
2024-11-03 12:18:16 +0200

    feat: add swagger documentation

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,14 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const port = process.env.PORT || 3000;
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
+      transform: true,
     })
   );
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('DB-2024 course API')
+    .setDescription('The DB-2024 course API description')
+    .setVersion('1.0')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  await app.listen(port);
+
+  console.info(`Started server on 127.0.0.1:${port}`);
 }
 bootstrap();
